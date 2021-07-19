9bf182b37f5c30de8f078baed0df839920a2b887
Ramesh P <85920413+rameshdrizzle1@users.noreply.github.com>
2021-07-19 12:25:20 +0530

    Used data source to get the appropriate AMI

--- main.tf
+++ main.tf
@@ -1,8 +1,30 @@
 provider "aws"{
-  region = "us-east-1"
-  access_key =""
   secret_key = ""
-  
+  access_key = ""
+  region = "us-east-1"
 }
 
-resourece "aws_instance_ec2" "pipelinetest"
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["self"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+
